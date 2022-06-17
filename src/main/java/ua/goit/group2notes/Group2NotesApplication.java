225924515fddb7c5a3ca75bb3389d90d7962693a
YuraCoupe <yurik124@gmail.com>
2022-06-17 11:21:28 +0300

    Initial commit

---
+++ src/main/java/ua/goit/group2notes/Group2NotesApplication.java
@@ -0,0 +1,13 @@
+package ua.goit.group2notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group2NotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group2NotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/goit/group2notes/Group2NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.goit.group2notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group2NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
