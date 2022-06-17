8222f074f6eff7a419499a59e443d65502e19670
rediska_pth <rediska_pth@i.ua>
2022-06-17 18:34:30 +0300

    Add note and user model-files

---
+++ src/main/java/ua/goit/note/NoteConverter.java
@@ -0,0 +1,25 @@
+package ua.goit.note;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoteConverter {
+
+    public NoteDto convert(NoteDao dao) {
+        NoteDto dto = new NoteDto();
+        dto.setId(dao.getId());
+        dto.setTitle(dao.getTitle());
+        dto.setText(dao.getText());
+        dto.setAccessType(dao.getAccessType());
+        return dto;
+    }
+
+    public NoteDao convert(NoteDto dto) {
+        NoteDao dao = new NoteDao();
+        dao.setId(dto.getId());
+        dao.setTitle(dto.getTitle());
+        dao.setText(dto.getText());
+        dao.setAccessType(dto.getAccessType());
+        return dao;
+    }
+}
---
+++ src/main/java/ua/goit/user/UserConverter.java
@@ -0,0 +1,42 @@
+package ua.goit.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ua.goit.note.NoteConverter;
+
+import java.util.stream.Collectors;
+
+@Service
+public class UserConverter {
+
+    private final NoteConverter noteConverter;
+
+    @Autowired
+    public UserConverter(NoteConverter noteConverter) {
+        this.noteConverter = noteConverter;
+    }
+
+    public UserDto convert(UserDao dao) {
+        UserDto dto = new UserDto();
+        dto.setId(dao.getId());
+        dto.setUsername(dao.getUsername());
+        dto.setPassword(dao.getPassword());
+        dto.setUserRole(dao.getUserRole());
+        dto.setNotes(dao.getNotes().stream()
+                .map(noteConverter::convert)
+                .collect(Collectors.toSet()));
+        return dto;
+    }
+
+    public UserDao convert(UserDto dto) {
+        UserDao dao = new UserDao();
+        dao.setId(dto.getId());
+        dao.setUsername(dto.getUsername());
+        dao.setPassword(dto.getPassword());
+        dao.setUserRole(dto.getUserRole());
+        dao.setNotes(dto.getNotes().stream()
+                .map(noteConverter::convert)
+                .collect(Collectors.toSet()));
+        return dao;
+    }
+}
