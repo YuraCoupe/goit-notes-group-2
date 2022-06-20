6b769cffd7b62e1846d8c7d71e83595df35bcd6f
YuraCoupe <yurik124@gmail.com>
2022-06-20 13:21:40 +0300

    add notes table

--- src/main/resources/db/migration/h2/V1_0__create_users_table.sql
+++ src/main/resources/db/migration/h2/V1_0__create_users_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS users (
 	id UUID DEFAULT random_uuid() PRIMARY KEY,
 	username varchar(50),
 	password varchar(200),
-	role varchar(50)
+	user_role varchar(50)
 );
 
 CREATE TABLE notes (
