efd848528d3fe100824ff2262d7db39a5d1fb831
YuraCoupe <yurik124@gmail.com>
2022-06-27 13:17:40 +0300

    add default user on application start if not exist

---
+++ src/main/java/ua/goit/group2notes/security/AppReadyListener.java
@@ -0,0 +1,53 @@
+package ua.goit.group2notes.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import ua.goit.group2notes.user.UserDto;
+import ua.goit.group2notes.user.UserRole;
+import ua.goit.group2notes.user.UserService;
+
+@Component
+public class AppReadyListener {
+    @Value("${DEFAULT_ADMIN_NAME}")
+    private String defaultAdminName;
+
+    @Value("${DEFAULT_ADMIN_PASSWORD}")
+    private String defaultAdminPassword;
+
+    private UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+public AppReadyListener(UserService userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void appReady() {
+        try {
+            if (userService.findUserByUsername(defaultAdminName) != null) {
+                return;
+            }
+        } catch (UsernameNotFoundException e) {
+            addAdminUser();
+        }
+    }
+
+    private void addAdminUser() {
+        UserDto adminUser = new UserDto();
+        adminUser.setUsername(defaultAdminName);
+        adminUser.setPassword(passwordEncoder.encode(defaultAdminPassword));
+        adminUser.setUserRole(UserRole.ROLE_ADMIN);
+
+        userService.save(adminUser);
+    }
+}
+
